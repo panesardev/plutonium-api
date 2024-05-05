cd4d7eadc1daa1d8f6116206da8efe88200bbf6f
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-05-04 23:21:40 -0400

    refactor

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
 export const BASE_URL = 'https://plutonium-api.vercel.app';
 
 export const DOMAIN = 'https://plutoniumx.vercel.app';
-
-export const FEATURED_SLUG = 'integrating-firebase-into-angular';
